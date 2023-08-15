b0f3b54694729d5f6d2f6ccc68357dae57113a64
zcabeto <zcabeto@ucl.ac.uk>
2023-08-15 18:29:35 +0100

    Create Item.java

---
+++ src/main2/Item.java
@@ -0,0 +1,25 @@
+package main2;
+
+abstract class Item {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String new_name) {
+        this.name = new_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String new_description) {
+        this.description = new_description;
+    }
+
+    abstract Object getData();      // retrieve info
+    abstract void voidData();       // refresh info
+}
