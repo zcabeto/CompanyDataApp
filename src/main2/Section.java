f504cfe6346ce73346f0afa4a6ee53401ca43206
zcabeto <zcabeto@ucl.ac.uk>
2023-08-26 11:57:55 +0100

    Section can set & reset data

--- src/main2/Section.java
+++ src/main2/Section.java
@@ -1,3 +1,20 @@
+package main2;
+
+public class Section extends ItemSublist
+{
+    private boolean keepData;       // should the system retain the data input once the day has passed?
+    private boolean keepSection;    // should the section still be recorded once the day has passed?
+
     public Section(String name) {
         super(name);
     }
+
+    public void resetData(){
+        if (!keepData){
+            voidData();
+        }
+    }
+    public boolean logSection(){
+        return keepSection;
+    }
+}
