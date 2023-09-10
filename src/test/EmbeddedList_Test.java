ce7cb8cbc68a869adaa882591071066e467ee576
zcabeto <zcabeto@ucl.ac.uk>
2023-09-10 10:57:48 +0100

    Update EmbeddedList_Test.java

--- src/test/EmbeddedList_Test.java
+++ src/test/EmbeddedList_Test.java
@@ -3,7 +3,7 @@ package test;
 import org.junit.Test;
 
 public class EmbeddedList_Test {
-    private final String list_name = "list_all";
+    /*private final String list_name = "list_all";
     private final ItemSublist embeddedList = new ItemSublist();
     private final Item testItem = new Item();
     private final ItemSublist testList = new ItemSublist();
@@ -53,5 +53,5 @@ public class EmbeddedList_Test {
         embeddedList.item_to_list(testItem);
         assert(embeddedList.getItems().get(0) instanceof EmbeddedList);
         embeddedList.removeItem(testItem);
-    }
+    }*/
 }
