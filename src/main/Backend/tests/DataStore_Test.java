3756b24860dd7593e30a12e4a3f4fea66c40f065
zcabeto <zcabeto@ucl.ac.uk>
2023-09-10 11:06:22 +0100

    added tests

--- src/main/Backend/tests/DataStore_Test.java
+++ src/main/Backend/tests/DataStore_Test.java
@@ -1,32 +1,54 @@
 package main.Backend.tests;
 
-import main.Backend.InfoLog.Checkbox;
+import main.Backend.InfoLog.DataStore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataStore_Test {
-    private Checkbox item;
+    private DataStore item;
     private String name = "item_name";
     private String description = "item_description";
+    private String data = "item_data";
 
     @Test
     public void constructorCorrectlySetsName(){
-        item = new Checkbox(name);
+        item = new DataStore(name);
         assertEquals(name,item.getName());
     }
 
     @Test
     public void setterCorrectlySetsName(){
-        item = new Checkbox("");
+        item = new DataStore("");
         item.setName(name);
         assertEquals(name,item.getName());
     }
 
     @Test
     public void setterCorrectlySetsDescription(){
-        item = new Checkbox(name);
+        item = new DataStore(name);
         item.setDescription(description);
         assertEquals(description,item.getDescription());
+    }
+
+    @Test
+    public void defaultIsEmpty(){
+        item = new DataStore(name);
+        assertEquals(item.getData(),"");
+    }
+
+    @Test
+    public void setDataWritesData(){
+        item = new DataStore(name);
+        item.setData(data);
+        assertEquals(item.getData(),data);
+    }
+
+    @Test
+    public void voidDataEmptiesData(){
+        item = new DataStore(name);
+        item.setData(data);
+        item.voidData();
+        assertEquals(item.getData(),"");
     }
 }
