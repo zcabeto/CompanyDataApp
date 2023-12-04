a505c3e12bef6f158918418f80574ab2c08a7f0a
zcabeto <zcabeto@ucl.ac.uk>
2023-12-04 18:09:22 +0000

    create items typescript

---
+++ data-log-app/src/app/main_classes/ItemTypes.tsx
@@ -0,0 +1,90 @@
+abstract class Item{
+    private name: string;
+    private description: string;
+
+    constructor(name: string){
+        this.name = name;
+        this.description = "";
+    }
+    getName():string{
+        return this.name;
+    }
+    setName(name: string):void{
+        this.name = name;
+    }
+    getDescription():string{
+        return this.description;
+    }
+    setDescription(description: string):void{
+        this.description = description;
+    }
+    abstract getData():Object;
+    abstract voidData():void;
+}
+
+class Checkbox extends Item{
+    private isChecked: boolean;
+
+    constructor(name: string){
+        super(name);
+        this.isChecked = false;
+    }
+
+    getData():boolean {
+        return this.isChecked;
+    }
+    checkbox():void {
+        this.isChecked = !this.isChecked;
+    }
+    voidData():void {
+        this.isChecked = false;
+    }
+}
+
+class DataStore extends Item {
+    private data: string;
+
+    constructor(name: string){
+        super(name);
+        this.data = "";
+    }
+
+    getData():string {
+        return this.data;
+    }
+    setData(data: string):void {
+        this.data = data;
+    }
+    voidData():void {
+        this.data = "";
+    }
+}
+
+class ItemSublist extends Item {
+    private list: Array<Item>;
+
+    constructor(name: string){
+        super(name);
+        this.list = new Array;
+    }
+
+    getData():Array<Item> {
+        return this.list;
+    }
+    addItem(item: Item):void {
+        this.list.push(item)
+    }
+    removeItem(item: Item):void {
+        const index = this.list.indexOf(item, 0);
+        if (index > -1) {
+            this.list.splice(index, 1);
+        }
+    }
+    voidData():void {
+        for (var i=0; i<this.list.length; i++){
+            this.list[i].voidData();
+        }
+    }
+}
+
+export {}
