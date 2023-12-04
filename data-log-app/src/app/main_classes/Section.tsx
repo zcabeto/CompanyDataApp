c7a4889cb3295ef1518738b6c9b9cf5d539a3eb6
zcabeto <zcabeto@ucl.ac.uk>
2023-12-04 22:33:03 +0000

    create section typescript

---
+++ data-log-app/src/app/main_classes/Section.tsx
@@ -0,0 +1,22 @@
+import ItemSublist from 'ItemTypes.tsx'
+
+class Section extends ItemSublist {
+    private isLogged: boolean;
+    private clearSection: boolean;
+
+    constructor(name:string, isLogged:boolean, clearSection:boolean){
+        super(name);
+        this.isLogged = isLogged;
+        this.clearSection = clearSection;
+    }
+    logSection():boolean {
+        return this.isLogged;
+    }
+    clear():boolean {
+        if (this.clearSection){
+            voidData();
+            return true;
+        }
+        return false;
+    }
+}
