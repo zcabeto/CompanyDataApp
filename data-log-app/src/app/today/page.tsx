6d0fe1d0b1b60b7625fa2e4d57e927d82ae46d62
zcabeto <zcabeto@ucl.ac.uk>
2024-01-04 11:14:29 +0000

    access DayLog Sections

--- data-log-app/src/app/today/page.tsx
+++ data-log-app/src/app/today/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import Link from 'next/link'
-import {LogHandler,DayLog,LocalDate} from '/./src/app/main_classes/backend.tsx'
+import {LogHandler,DayLog,LocalDate, Section} from './/../main_classes/backend.tsx'
 
 const log:LogHandler = new LogHandler();
+const today:DayLog = log.getLog(true,null);
+today.sections.push(new Section("init",true,true));
 export function whichPage(){
   return (
     <h1 className="text-3xl font-bold">
@@ -11,12 +13,32 @@ export function whichPage(){
   )
 }
 
-function getDayLog(): DayLog {
-  return log.getLog(true,null);
+export function addSectionExport(){
+  // must create a new empty section and link to section page
+  return (
+    <div>
+      <button
+            onClick={() => {
+              today.sections.push(new Section("name",true,true));
+            }}
+          >
+          AddSection
+      </button>
+    </div>
+  )
+}
+export function editSection(){
+  // must link to section page with this section
 }
 
-export default async function Page() {
-  const day: DayLog = getDayLog();
-  const key = day.toString();
-  return (<>{whichPage()} {key}</>)
+export function getSections():string {
+  var key:string = "";
+  for (var i=0; i<today.sections.length; i++){
+    key = key.concat(today.sections[i].getName());
+  }
+  return key
+}
+
+export default function Page(){
+  return (<>{whichPage()} {addSectionExport()} {getSections()}</>)
 }
